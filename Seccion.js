5167996e73722faa0cef7726f6fcb7d8c5cfd56a
IngGeolAsist <80356680+IngGeolAsist@users.noreply.github.com>
2021-10-06 11:49:12 -0500

    Add files via upload

---
+++ Seccion.js
@@ -0,0 +1,8 @@
+var Seccion = {
+"type": "FeatureCollection",
+"name": "Seccion",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "FID": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -98.527550230085041, 18.422744615400525 ], [ -98.291409686769711, 18.422010452358801 ] ] ] } }
+]
+};
