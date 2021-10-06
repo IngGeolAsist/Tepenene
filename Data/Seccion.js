ea0fd195bd5d6b6d8732a6d112be8da019f3e22b
IngGeolAsist <80356680+IngGeolAsist@users.noreply.github.com>
2021-10-06 11:30:46 -0500

    Delete Seccion.js

--- Data/Seccion.js
+++
@@ -1,8 +0,0 @@
-{
-"type": "FeatureCollection",
-"name": "Seccion",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "FID": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -98.527550230085041, 18.422744615400525 ], [ -98.291409686769711, 18.422010452358801 ] ] ] } }
-]
-}
